969bbc6773725170317a0b6c31e0b3608522e52a
Michael Dyrynda <michael@dyrynda.com.au>
2019-10-12 22:16:56 +1030

    Add tests to ensure webhooks are sent when scheduled task events are fired

---
+++ tests/ScheduledTaskListenerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Thenpingme\Tests;
+
+use Illuminate\Console\Events\ScheduledTaskFinished;
+use Illuminate\Console\Events\ScheduledTaskStarting;
+use Illuminate\Console\Scheduling\Event;
+use Illuminate\Console\Scheduling\EventMutex;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Queue;
+use Spatie\WebhookServer\CallWebhookJob;
+
+class ScheduledTaskListenerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Config::set([
+            'thenpingme.project_id' => 'abc123',
+            'thenpingme.signing_key' => 'super-secret',
+        ]);
+
+        Queue::fake();
+    }
+
+    /** @test */
+    public function it_listens_for_a_scheduled_task_starting()
+    {
+        $event = new Event(
+            $this->mock(EventMutex::class),
+            'artisan thenpingme:testing'
+        );
+
+        tap(app(Dispatcher::class), function ($dispatcher) use ($event) {
+            $dispatcher->dispatch(new ScheduledTaskStarting($event));
+        });
+
+        Queue::assertPushed(CallWebhookJob::class, function ($job) use ($event) {
+            $this->assertEquals('https://thenping.me/api/projects/abc123/ping', $job->webhookUrl);
+
+            return true;
+        });
+    }
+
+    /** @test */
+    public function it_listens_for_a_scheduled_task_finishing()
+    {
+        $event = new Event(
+            $this->mock(EventMutex::class),
+            'artisan thenpingme:testing'
+        );
+
+        tap(app(Dispatcher::class), function ($dispatcher) use ($event) {
+            $dispatcher->dispatch(new ScheduledTaskFinished($event, 1));
+        });
+
+        Queue::assertPushed(CallWebhookJob::class, function ($job) use ($event) {
+            $this->assertEquals('https://thenping.me/api/projects/abc123/ping', $job->webhookUrl);
+
+            return true;
+        });
+    }
+}
