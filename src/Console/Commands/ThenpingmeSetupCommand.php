7d7f2d64c0a5c6805cf78badca011e70ee705614
Michael Dyrynda <michael@dyrynda.com.au>
2019-10-14 21:18:16 +1030

    Add functionality to send initial tasks in setup command

--- src/Console/Commands/ThenpingmeSetupCommand.php
+++ src/Console/Commands/ThenpingmeSetupCommand.php
@@ -3,26 +3,40 @@
 namespace Thenpingme\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use Thenpingme\Facades\Thenpingme;
+use Thenpingme\Payload\ThenpingmeSetupPayload;
 
 class ThenpingmeSetupCommand extends Command
 {
+    protected $description = 'Configure your application to report scheduled tasks to ThenPing.me automatically.';
+
+    protected $schedule;
+
     protected $signature = 'thenpingme:setup {project_id : The UUID of the ThenPing.me project you are setting up}';
 
-    protected $description = 'Configure your application to report scheduled tasks to ThenPing.me automatically.';
+    protected $thenpingme;
 
     public function __construct()
     {
         parent::__construct();
     }
 
-    public function handle(): void
+    public function handle(Schedule $schedule, Thenpingme $thenpignme): void
     {
-        $this->updateEnv();
+        $this->schedule = $schedule;
+        $this->thenpignme = $thenpignme;
+
+        $this->updateConfig();
+        $this->setupInitialTasks();
     }
 
-    protected function updateEnv(): void
+    protected function updateConfig(): void
     {
         tap(new Filesystem, function ($filesystem) {
             $path = base_path('.example.env');
@@ -34,10 +48,35 @@ class ThenpingmeSetupCommand extends Command
 
         tap(new Filesystem, function ($filesystem) {
             $path = base_path('.env');
+            $key = Thenpingme::generateSigningKey();
 
             $filesystem->append($path, sprintf('THENPINGME_PROJECT_ID=%s%s', $this->argument('project_id'), PHP_EOL));
-            $filesystem->append($path, 'THENPINGME_SIGNING_KEY='.Thenpingme::generateSigningKey().PHP_EOL);
+            $filesystem->append($path, 'THENPINGME_SIGNING_KEY='.$key.PHP_EOL);
             $filesystem->append($path, 'THENPINGME_QUEUE_PING=false'.PHP_EOL);
+
+            Config::set([
+                'thenpingme.project_id' => $this->argument('project_id'),
+                'thenpingme.signing_key' => $key,
+            ]);
         });
+
+        if (file_exists(app()->getCachedConfigPath())) {
+            Artisan::call('config:cache');
+        }
+    }
+
+    protected function setupInitialTasks(): void
+    {
+        Thenpingme::setup()->payload(
+            ThenpingmeSetupPayload::make($this->scheduledTasks())->toArray()
+        )->dispatch();
+    }
+
+    protected function scheduledTasks(): array
+    {
+        return collect($this->schedule->events())->filter(function ($event) {
+            return App::environment($event->environments)
+                || empty($event->environments);
+        })->toArray();
     }
 }
