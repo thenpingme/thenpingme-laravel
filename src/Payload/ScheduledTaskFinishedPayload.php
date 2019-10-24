c49c2d575bd4fe6c28c85efb9b006e1035ca7dcb
Michael Dyrynda <michael@dyrynda.com.au>
2019-10-24 20:15:40 +1030

    Use the class basename, rather than hardcoded string, as the ping type

--- src/Payload/ScheduledTaskFinishedPayload.php
+++ src/Payload/ScheduledTaskFinishedPayload.php
@@ -9,7 +9,7 @@ class ScheduledTaskFinishedPayload extends ThenpingmePayload
     public function toArray(): array
     {
         return array_merge(parent::toArray(), [
-            'type' => 'finished',
+            'type' => class_basename($this->event),
             'time' => Carbon::now()->toIso8601String(),
             'runtime' => sprintf('%.2fs', $this->event->runtime),
             'exit_code' => $this->event->task->exitCode,
--- src/Payload/ScheduledTaskStartingPayload.php
+++ src/Payload/ScheduledTaskStartingPayload.php
@@ -9,7 +9,7 @@ class ScheduledTaskStartingPayload extends ThenpingmePayload
     public function toArray(): array
     {
         return array_merge(parent::toArray(), [
-            'type' => 'starting',
+            'type' => class_basename($this->event),
             'time' => Carbon::now()->toIso8601String(),
             'expires' => Carbon::now()->addMinutes($this->event->task->expiresAt)->toIso8601String(),
         ]);
