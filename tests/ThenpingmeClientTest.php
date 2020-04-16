98586dd16d9f796bdda562ec7c8744254f8da2fc
Michael Dyrynda <michael@dyrynda.com.au>
2020-04-16 23:16:06 +0930

    Remove use of deprecated phpunit method

--- tests/ThenpingmeClientTest.php
+++ tests/ThenpingmeClientTest.php
@@ -20,7 +20,7 @@ class ThenpingmeClientTest extends TestCase
         config(['thenpingme.api_url' => null]);
 
         $this->expectException(CouldNotSendPing::class);
-        $this->expectExceptionMessageRegExp('/base URL is not set/');
+        $this->expectExceptionMessageMatches('/base URL is not set/');
 
         app(Client::class)->payload(['thenpingme' => 'test'])->ping()->dispatch();
     }
@@ -31,7 +31,7 @@ class ThenpingmeClientTest extends TestCase
         config(['thenpingme.signing_key' => null]);
 
         $this->expectException(CouldNotSendPing::class);
-        $this->expectExceptionMessageRegExp('/signing secret is not set/');
+        $this->expectExceptionMessageMatches('/signing secret is not set/');
 
         app(Client::class)->payload(['thenpingme' => 'test'])->ping()->dispatch();
     }
@@ -40,7 +40,7 @@ class ThenpingmeClientTest extends TestCase
     public function it_does_not_send_a_ping_if_endpoint_is_missing()
     {
         $this->expectException(CouldNotSendPing::class);
-        $this->expectExceptionMessageRegExp('/endpoint URL is not set/');
+        $this->expectExceptionMessageMatches('/endpoint URL is not set/');
 
         app(Client::class)->payload(['thenpingme' => 'test'])->dispatch();
     }
