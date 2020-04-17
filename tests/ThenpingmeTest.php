0b4b919a5d75583fe932a6862162f2249ac942cd
Michael Dyrynda <michael@dyrynda.com.au>
2020-04-17 14:02:28 +0930

    tap, tap, tap

--- tests/ThenpingmeTest.php
+++ tests/ThenpingmeTest.php
@@ -6,16 +6,17 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Arr;
 use Thenpingme\Facades\Thenpingme;
 use Thenpingme\Payload\TaskPayload;
-use Thenpingme\Tests\Fixtures\SomeJob;
+use Thenpingme\Tests\Fixtures\InvokableJob;
 
 class ThenpingmeTest extends TestCase
 {
     /** @test */
     public function it_determines_non_unique_tasks()
     {
-        $schedule = $this->app->make(Schedule::class);
-        $schedule->command('test:command')->hourly();
-        $schedule->command('test:command')->hourly();
+        tap($this->app->make(Schedule::class), function ($schedule) {
+            $schedule->command('test:command')->hourly();
+            $schedule->command('test:command')->hourly();
+        });
 
         tap(Thenpingme::scheduledTasks()->collisions(), function ($tasks) {
             $this->assertCount(2, $tasks);
@@ -26,27 +27,29 @@ class ThenpingmeTest extends TestCase
     /** @test */
     public function it_determines_non_unique_jobs()
     {
-        $schedule = $this->app->make(Schedule::class);
-        $schedule->job(SomeJob::class)->hourly();
-        $schedule->job(SomeJob::class)->hourly();
+        tap($this->app->make(Schedule::class), function ($schedule) {
+            $schedule->job(SomeJob::class)->hourly();
+            $schedule->job(SomeJob::class)->hourly();
+        });
 
         tap(Thenpingme::scheduledTasks()->collisions(), function ($tasks) {
             $this->assertCount(2, $tasks);
-            $this->assertNull($tasks[0]['extra']);
+            $this->assertRegExp('/^Line [0-9]+ to [0-9]+ of/', $tasks[0]['extra']);
         });
     }
 
     /** @test */
     public function it_determines_closures_are_non_unique_tasks()
     {
-        $schedule = $this->app->make(Schedule::class);
-        $schedule->call(function () {
-            // This task does one thing
-        })->everyMinute();
+        tap($this->app->make(Schedule::class), function ($schedule) {
+            $schedule->call(function () {
+                // This task does one thing
+            })->everyMinute();
 
-        $schedule->call(function () {
-            // This task does another thing
-        })->everyMinute();
+            $schedule->call(function () {
+                // This task does another thing
+            })->everyMinute();
+        });
 
         tap(Thenpingme::scheduledTasks()->collisions(), function ($tasks) {
             $this->assertCount(2, $tasks);
@@ -68,9 +71,18 @@ class ThenpingmeTest extends TestCase
         });
     }
 
-        $schedule->call(function () {
-            // This task does another thing
-        })->everyMinute()->description('second task description');
+    /** @test */
+    public function it_determines_closures_with_unique_descriptions_are_unique()
+    {
+        tap($this->app->make(Schedule::class), function ($schedule) {
+            $schedule->call(function () {
+                // This task does one thing
+            })->everyMinute()->description('first task description');
+
+            $schedule->call(function () {
+                // This task does another thing
+            })->everyMinute()->description('second task description');
+        });
 
         $this->assertEmpty(Thenpingme::scheduledTasks()->collisions());
     }
@@ -78,9 +90,10 @@ class ThenpingmeTest extends TestCase
     /** @test */
     public function it_determines_jobs_with_unique_intervals_are_unique()
     {
-        $schedule = $this->app->make(Schedule::class);
-        $schedule->job(SomeJob::class)->hourly();
-        $schedule->job(SomeJob::class)->weekly();
+        tap($this->app->make(Schedule::class), function ($schedule) {
+            $schedule->job(SomeJob::class)->hourly();
+            $schedule->job(SomeJob::class)->weekly();
+        });
 
         $this->assertEmpty(Thenpingme::scheduledTasks()->collisions());
     }
@@ -88,10 +101,11 @@ class ThenpingmeTest extends TestCase
     /** @test */
     public function it_can_fingerprint_a_closure_task()
     {
-        $schedule = $this->app->make(Schedule::class);
-        $schedule->call(function () {
-            // some task
-        })->everyMinute()->description('some task');
+        tap($this->app->make(Schedule::class), function ($schedule) {
+            $schedule->call(function () {
+                // some task
+            })->everyMinute()->description('some task');
+        });
 
         $this->assertEquals(
             'thenpingme:'.sha1('* * * * *.some task'),
