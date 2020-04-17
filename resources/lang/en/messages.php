536d8ffadd45dbbf5cc11cd6db56f20444ebd0ee
Michael Dyrynda <michael@dyrynda.com.au>
2020-04-17 12:45:28 +0930

    Move setup text to translation file

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
+    'setup' => [
+        'signing_key' => 'Generate signing key',
+        'write_env' => 'Write configuration to the .env file',
+        'write_env_example' => 'Write configuration to the .env.example file',
+        'public_config' => 'Publish config file',
+    ],
+
+    'env_missing' => 'The .env file is missing. Please add the following lines to your configuration, then run:',
+
+    'initial_setup' => 'Setting up initial tasks with :url',
+
     'healthy_tasks' => 'Your tasks are correctly configured and can be synced to thenping.me!',
 
     'indistinguishable_tasks' => 'Tasks have been identified that are not uniquely distinguishable!',
