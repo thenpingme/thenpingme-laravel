ccfb029da4f347c229159b8716550ed384847cea
Michael Dyrynda <michael@dyrynda.com.au>
2020-08-02 11:41:18 +0930

    add rector config, composer-normalize

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('php_version_features', '7.2');
+
+    $services = $containerConfigurator->services();
+};
