2274be7150996d43fc8bd870c1e51b1b3da5cade
Michael Dyrynda <michael@dyrynda.com.au>
2020-11-17 15:54:30 +1030

    use Str::is, rather than contains, for correctness

--- src/TaskIdentifier.php
+++ src/TaskIdentifier.php
@@ -27,7 +27,7 @@ class TaskIdentifier
                 return static::TYPE_CLOSURE;
             }
 
-            if (Str::of($task->getSummaryForDisplay())->contains(['Closure', 'Callback'])) {
+            if (Str::of($task->getSummaryForDisplay())->is(['Closure', 'Callback'])) {
                 return static::TYPE_CLOSURE;
             }
         }
