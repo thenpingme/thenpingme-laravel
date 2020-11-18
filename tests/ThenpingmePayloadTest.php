thenpingme/thenpingme-laravel

commit f1ba129d49d4bf0b0820af381b671b2bb1234f16

support schedule-specific timezones

src/Payload/TaskPayload.php
@@ -23,6 +23,7 @@ class TaskPayload extends ThenpingmePayload
     public function toArray(): array
     {
         return [
+            'timezone' => $this->task->timezone,
             'release' => config('thenpingme.release'),
             'type' => (new TaskIdentifier)($this->task),
             'expression' => $this->task->expression,

tests/ThenpingmePayloadTest.php
@@ -42,6 +42,7 @@ class ThenpingmePayloadTest extends TestCase
 
         tap(ThenpingmePayload::fromTask($task)->toArray(), function ($payload) use ($task) {
             Assert::assertArraySubset([
+                'timezone' => 'UTC',
                 'type' => TaskIdentifier::TYPE_COMMAND,
                 'expression' => '* * * * *',
                 'command' => 'generate:payload',
@@ -68,6 +69,7 @@ class ThenpingmePayloadTest extends TestCase
 
         tap(ThenpingmePayload::fromTask($task)->toArray(), function ($payload) use ($task) {
             Assert::assertArraySubset([
+                'timezone' => 'UTC',
                 'type' => TaskIdentifier::TYPE_COMMAND,
                 'expression' => '* * * * *',
                 'command' => 'thenpingme:filtered',
@@ -92,6 +94,7 @@ class ThenpingmePayloadTest extends TestCase
 
         tap(ThenpingmePayload::fromTask($task)->toArray(), function ($payload) use ($task) {
             Assert::assertArraySubset([
+                'timezone' => 'UTC',
                 'type' => TaskIdentifier::TYPE_COMMAND,
                 'expression' => '* * * * *',
                 'command' => 'thenpingme:background',
@@ -169,6 +172,7 @@ class ThenpingmePayloadTest extends TestCase
             $this->assertInstanceOf(ScheduledTaskStartingPayload::class, $payload);
 
             tap($payload->toArray(), function ($body) use ($payload) {
+                $this->assertEquals('UTC', $body['task']['timezone']);
                 $this->assertEquals($payload->fingerprint(), $body['fingerprint']);
                 $this->assertEquals('10.1.1.1', $body['ip']);
                 $this->assertEquals(gethostname(), $body['hostname']);
@@ -269,6 +273,7 @@ class ThenpingmePayloadTest extends TestCase
             $this->assertInstanceOf(ScheduledTaskSkippedPayload::class, $payload);
 
             tap($payload->toArray(), function ($body) use ($payload) {
+                $this->assertEquals('UTC', $body['task']['timezone']);
                 $this->assertEquals($payload->fingerprint(), $body['fingerprint']);
                 $this->assertEquals('10.1.1.1', $body['ip']);
                 $this->assertEquals(gethostname(), $body['hostname']);

commit d44c77bcf0e9969a66f6c3bff7f4e39a0e62d528

Fix bug introduced by f1ba129d to ensure the offset string is used always

src/Payload/TaskPayload.php
@@ -2,6 +2,7 @@
 
 namespace Thenpingme\Payload;
 
+use Illuminate\Support\Facades\Date;
 use ReflectionClass;
 use Thenpingme\Facades\Thenpingme;
 use Thenpingme\TaskIdentifier;
@@ -23,7 +24,7 @@ class TaskPayload extends ThenpingmePayload
     public function toArray(): array
     {
         return [
-            'timezone' => $this->task->timezone,
+            'timezone' => Date::now($this->task->timezone)->getOffsetString(),
             'release' => config('thenpingme.release'),
             'type' => (new TaskIdentifier)($this->task),
             'expression' => $this->task->expression,

tests/ThenpingmePayloadTest.php
@@ -42,7 +42,7 @@ class ThenpingmePayloadTest extends TestCase
 
         tap(ThenpingmePayload::fromTask($task)->toArray(), function ($payload) use ($task) {
             Assert::assertArraySubset([
-                'timezone' => 'UTC',
+                'timezone' => '+00:00',
                 'type' => TaskIdentifier::TYPE_COMMAND,
                 'expression' => '* * * * *',
                 'command' => 'generate:payload',
@@ -69,7 +69,7 @@ class ThenpingmePayloadTest extends TestCase
 
         tap(ThenpingmePayload::fromTask($task)->toArray(), function ($payload) use ($task) {
             Assert::assertArraySubset([
-                'timezone' => 'UTC',
+                'timezone' => '+00:00',
                 'type' => TaskIdentifier::TYPE_COMMAND,
                 'expression' => '* * * * *',
                 'command' => 'thenpingme:filtered',
@@ -94,7 +94,7 @@ class ThenpingmePayloadTest extends TestCase
 
         tap(ThenpingmePayload::fromTask($task)->toArray(), function ($payload) use ($task) {
             Assert::assertArraySubset([
-                'timezone' => 'UTC',
+                'timezone' => '+00:00',
                 'type' => TaskIdentifier::TYPE_COMMAND,
                 'expression' => '* * * * *',
                 'command' => 'thenpingme:background',
@@ -172,7 +172,7 @@ class ThenpingmePayloadTest extends TestCase
             $this->assertInstanceOf(ScheduledTaskStartingPayload::class, $payload);
 
             tap($payload->toArray(), function ($body) use ($payload) {
-                $this->assertEquals('UTC', $body['task']['timezone']);
+                $this->assertEquals('+00:00', $body['task']['timezone']);
                 $this->assertEquals($payload->fingerprint(), $body['fingerprint']);
                 $this->assertEquals('10.1.1.1', $body['ip']);
                 $this->assertEquals(gethostname(), $body['hostname']);
@@ -273,7 +273,7 @@ class ThenpingmePayloadTest extends TestCase
             $this->assertInstanceOf(ScheduledTaskSkippedPayload::class, $payload);
 
             tap($payload->toArray(), function ($body) use ($payload) {
-                $this->assertEquals('UTC', $body['task']['timezone']);
+                $this->assertEquals('+00:00', $body['task']['timezone']);
                 $this->assertEquals($payload->fingerprint(), $body['fingerprint']);
                 $this->assertEquals('10.1.1.1', $body['ip']);
                 $this->assertEquals(gethostname(), $body['hostname']);
@@ -295,6 +295,37 @@ class ThenpingmePayloadTest extends TestCase
             $this
                 ->app
                 ->makeWith(Schedule::class, ['+10:30'])
+                ->command('thenpingme:first')
+                ->description('This is the first task'),
+            1
+        );
+
+        tap(ThenpingmePayload::fromEvent($event), function ($payload) {
+            $this->assertInstanceOf(ScheduledTaskSkippedPayload::class, $payload);
+
+            tap($payload->toArray(), function ($body) use ($payload) {
+                $this->assertEquals('+10:30', $body['task']['timezone']);
+                $this->assertEquals($payload->fingerprint(), $body['fingerprint']);
+                $this->assertEquals('10.1.1.1', $body['ip']);
+                $this->assertEquals(gethostname(), $body['hostname']);
+                $this->assertEquals('ScheduledTaskSkipped', $body['type']);
+                $this->assertEquals('2019-10-11T00:00:00+00:00', $body['time']);
+                $this->assertEquals(app()->environment(), $body['environment']);
+            });
+        });
+    }
+
+    /** @test */
+    public function it_converts_string_timezones_to_utc_offset()
+    {
+        Carbon::setTestNow('2019-10-11 00:00:00', 'UTC');
+
+        config(['app.schedule_timezone' => 'Australia/Adelaide']);
+
+        $event = new ScheduledTaskSkipped(
+            $this
+                ->app
+                ->makeWith(Schedule::class, ['Australia/Adelaide'])
                 ->command('thenpingme:first')
                 ->description('This is the first task'),
             1