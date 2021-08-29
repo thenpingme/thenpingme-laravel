d8fee934a42c7f127be07ba134018a10f2e5cd40
Michael Dyrynda <michael@dyrynda.com.au>
2021-08-29 12:11:44 +0930

    swap global app helper to App facade

--- src/Exceptions/CouldNotSendPing.php
+++ src/Exceptions/CouldNotSendPing.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Thenpingme\Exceptions;
 
+use Illuminate\Support\Facades\App;
 use RuntimeException;
 
 final class CouldNotSendPing extends RuntimeException
@@ -11,21 +12,21 @@ final class CouldNotSendPing extends RuntimeException
     public static function missingBaseUrl(): CouldNotSendPing
     {
         return new self(
-            app('translator')->get('thenpingme::translations.missing_base_url')
+            App::make('translator')->get('thenpingme::translations.missing_base_url')
         );
     }
 
     public static function missingUrl(): CouldNotSendPing
     {
         return new self(
-            app('translator')->get('thenpingme::translations.missing_endpoint_url')
+            App::make('translator')->get('thenpingme::translations.missing_endpoint_url')
         );
     }
 
     public static function missingSigningSecret(): CouldNotSendPing
     {
         return new self(
-            app('translator')->get('thenpingme::translations.missing_signing_secret')
+            App::make('translator')->get('thenpingme::translations.missing_signing_secret')
         );
     }
 }
--- src/Exceptions/InvalidSigner.php
+++ src/Exceptions/InvalidSigner.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Thenpingme\Exceptions;
 
+use Illuminate\Support\Facades\App;
 use InvalidArgumentException;
 use Thenpingme\Signer\Signer;
 
@@ -11,7 +12,7 @@ final class InvalidSigner extends InvalidArgumentException
 {
     public static function doesntImplementSigner(string $signingClass): InvalidSigner
     {
-        return new self(app('translator')->get('thenpingme::translations.invalid_signer', [
+        return new self(App::make('translator')->get('thenpingme::translations.invalid_signer', [
             'concrete' => $signingClass,
             'contract' => Signer::class,
         ]));
