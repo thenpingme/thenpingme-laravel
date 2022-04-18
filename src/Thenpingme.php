67bcf922661426fff2e33a721db47eabee0ae791
Michael Dyrynda <michael@dyrynda.com.au>
2022-04-19 08:23:51 +0930

    bump package version

--- src/Thenpingme.php
+++ src/Thenpingme.php
@@ -18,7 +18,7 @@ use Thenpingme\Collections\ScheduledTaskCollection;
 class Thenpingme
 {
     /** @var string */
-    public const VERSION = '3.1.0';
+    public const VERSION = '3.2.0';
 
     /** @var int */
     public const STORE_OUTPUT = 1;
