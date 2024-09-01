87f4e6548f493d0b4e19a4d7d5bcf5b24a9b601e
Michael Dyrynda <michael@dyrynda.com.au>
2024-09-01 10:53:03 +0930

    fix version

--- src/Thenpingme.php
+++ src/Thenpingme.php
@@ -18,7 +18,7 @@ use Thenpingme\Collections\ScheduledTaskCollection;
 class Thenpingme
 {
     /** @var string */
-    public const VERSION = '3.6.0';
+    public const VERSION = '4.x-dev';
 
     /** @var int */
     public const STORE_OUTPUT = 1;
